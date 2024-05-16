3968738081075b4efa0fbd4ed937f16d32ef2272
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-16 22:35:30 +0200

    add canvas and test object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+
 function App() {
   return (
-    <div className="App">
-	<p>hello world</p>
-    </div>
+    <>
+      <Canvas>
+        <mesh>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
 
