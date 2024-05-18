ee5d1cae9f5cf3a907d5765324b5397abbcd0855
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-18 15:03:01 +0200

    change api call from localhost to instance ip

--- src/apis/configs/axiosConfigs.js
+++ src/apis/configs/axiosConfigs.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "http://3.225.226.55:4000/api/",
 })
 
 // defining a custom error handler for all APIs
