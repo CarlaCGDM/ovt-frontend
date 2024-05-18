f691b377028d243895b4b9224cd7142b3c497f1d
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-18 15:03:01 +0200

    change api call from localhost to instance ip

--- src/components/TestSelector.js
+++ src/components/TestSelector.js
@@ -4,8 +4,6 @@ import { ExhibitAPI } from "../apis/ExhibitAPI";
 function TestSelector() {
   const [exhibitListLoading, setExhibitListLoading] = useState(false);
   const [exhibitList, setExhibitList] = useState([]);
-  const [selectedExhibitName, setSelectedExhibitName] = useState();
-  const [selectedExhibit, setSelectedExhibit] = useState();
 
   useEffect(() => {
     setExhibitListLoading(true);
@@ -13,53 +11,18 @@ function TestSelector() {
     ExhibitAPI.get()
       .then((exhibitList) => {
         setExhibitList(exhibitList);
-        setSelectedExhibitName(exhibitList[0]?.name);
       })
       .finally(() => {
         setExhibitListLoading(false);
       });
   }, []);
 
-  useEffect(() => {
-    if (selectedExhibitName) {
-      // enabling request cancellation
-      ExhibitAPI.get(selectedExhibitName, true).then((exhibit) => {
-        setSelectedExhibit(exhibit);
-      });
-    }
-  }, [selectedExhibitName]);
-
   console.log(exhibitList)
 
   return (
-    <div style={{ textAlign: "center", maxWidth: "600px", margin: "auto" }}>
-        <div>
-          <div
-            value={selectedExhibitName}
-            onChange={(e) => {
-              setSelectedExhibitName(e.target.value);
-            }}
-          >
-            {exhibitList.map((exhibit) => {
-              return (
-                <input type="radio" value={exhibit.name} key={exhibit.name}>
-                  {exhibit.name}
-                </input>
-              );
-            })}
-          </div>
-          {selectedExhibit && (
-            <div style={{ marginTop: "20px" }} title={selectedExhibit.name}>
-              {selectedExhibit.sprites.front_default && (
-                <img
-                  src={selectedExhibit.sprites.front_default}
-                  alt={selectedExhibit.name}
-                />
-              )}
-            </div>
-          )}
-        </div>
-      </div>
+    <>
+        <div>Hello.</div>
+    </>
   );
 }
 
