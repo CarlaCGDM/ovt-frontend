d1a81013a7a2956d112856f8b0ba0ab916dcc226
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2024-05-18 12:48:04 +0200

    added api layer

--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ function App() {
           <torusKnotGeometry />
           <meshNormalMaterial />
         </mesh>
-        <mesh position={[1,1,1]}>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
-        <mesh position={[2,2,2]}>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
       </Canvas>
     </>
   );
